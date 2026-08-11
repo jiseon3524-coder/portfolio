810a03a3c5bb68e2f2198063f006bca4c5b55b3e
Youn Jiseon <jiseon3524@gmail.com>
2026-08-11 19:10:36 +0900

    Update RoomManager.cs

--- Games/BBACKDO/Codes/Room/RoomManager.cs
+++ Games/BBACKDO/Codes/Room/RoomManager.cs
@@ -7,13 +7,15 @@ public class RoomManager : MonoBehaviour
     [SerializeField] private CameraController cameraController;
     [SerializeField] private Room startRoom;
 
+    // 지도 UI 스크립트
     [Header("지도 UI")]
     [SerializeField] private MapUI mapUI;
 
+    // 현재 방, 다른 스크립트에서 읽을 수 있도록
     private Room currentRoom;
-
     public Room CurrentRoom => currentRoom;
 
+    // 게임 시작 시, 컴포넌트 연결이 안 되어 있을 때를 위한 안전 코드
     private void Start()
     {
         if (player == null)
@@ -50,13 +52,16 @@ public class RoomManager : MonoBehaviour
             );
         }
 
+        // 시작방에서 시작하도록
         EnterRoom(startRoom);
     }
 
+    // 연결된 타겟 룸으로 이동하는 함수 + 컴포넌트 연결 안전코드
     public void MoveRoom(
         Room targetRoom,
         Transform spawnPoint
     )
+    
     {
         if (targetRoom == null)
         {
@@ -76,16 +81,13 @@ public class RoomManager : MonoBehaviour
             return;
         }
 
-        if (currentRoom != null)
-        {
-            currentRoom.SetMonstersActive(false);
-        }
-
+        // 플레이어가 각 방의 스폰포인트에서 시작하도록
         player.position = spawnPoint.position;
-
+        // 타겟룸으로 들어갈 때의 검사 함수
         EnterRoom(targetRoom);
     }
 
+    // 다른 룸으로 이동할 때 필요한 조정 ( 카메라전환, 몬스터활성화, 맵UI처리 )
     private void EnterRoom(Room room)
     {
         if (room == null)
